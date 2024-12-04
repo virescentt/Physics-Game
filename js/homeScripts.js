99986dd97d6108805be306d7fa0d4694158a4a3a
Virescent <selfvirescent@gmail.com>
2024-12-04 01:44:58 +0100

    WTF with adaptive on the cards? Im gonna deploy the site rn using Vercel, and check the adaptive directly through the phone

--- js/homeScripts.js
+++ js/homeScripts.js
@@ -33,30 +33,18 @@ function homeCardGeneration () {
 //     console.log('ну я впрочем смотрю, но ничего не вижу!!');
 // });
 
-/* Event listener */
-const author = document.querySelector(".author");
-console.log(author);
-
-author.addEventListener('click', function () {
-    console.log('Да, я Virescent');
-});
-
-/* Function */
-function doThing(){
-   alert('Horray! Someone wrote!');
-}
 
 document.getElementById('searchInput').addEventListener('input', function () {
     const query = this.value;
     const cards = document.querySelectorAll('.card');
-    console.log('ну я впрочем смотрю, но ничего не вижу!!');
+    console.log('Im looking..');
     cards.forEach(card => {
 
         const id = card.getAttribute('data-id');
 
         // Проверяем совпадение с query и показываем/скрываем
         if (id.startsWith(query)) {
-            card.style.display = 'block';
+            card.style.display = 'inline-block';
         } else {
             card.style.display = 'none';
         }
